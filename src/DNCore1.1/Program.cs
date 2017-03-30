f4aa94bebd1bceaaac16f0db0218993efdf05209
Mat Gutschow <matgutschow@gmail.com>
2017-03-30 08:09:42 -0500

    init commit

---
+++ src/DNCore1.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace DNCore1._1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseEnvironment("Development")
+                .UseStartup<Startup>()
+                .UseUrls("http://*:5000")
+                .Build();
+
+            host.Run();
+        }
+    }
+}
